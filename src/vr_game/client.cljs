753182c0dab411a29bb3cceb57d291b5c8db367e
keeper <zhurov.andrew@gmail.com>
2019-05-11 17:12:32 +0100

    client message receive WORKZ!

--- src/vr_game/client.cljs
+++ src/vr_game/client.cljs
@@ -1,23 +1,24 @@
 (ns vr-game.client
   (:require [reagent.core :as reagent :refer [atom]]))
 
-(def ws (js/WebSocket. "ws://localhost:8080"))
+(def ws (js/WebSocket. "ws://localhost:8081"))
 
 ;;192.168.1.255:8080
 
 (def msgs (atom ["hi" "hello" "i like food"]))
 
-(set! (.-onmessage ws) (fn [msg] 
-                         (swap! msgs conj msg)))
+(set! (.-onmessage ws) (fn [msg]
+                         (swap! msgs conj (.-data msg))))
 
 (defn hello-world []
   [:div
-   [:a-assets
+   #_[:a-assets
     [:img {:src "https://res.cloudinary.com/dexhx07lb/image/upload/v1557585843/5a4bc99c2da5ad73df7efe66.png" :id "person"}]]
    [:a-scene
-    (for [msg @msgs]
-      (def i (dec i))
-      [:a-text {:color "black" :value  msg  :position (str "-1 " i " -2")}])
+    (map-indexed (fn [idx msg]
+                   ^{:key idx} ;; supply id for each element, a way to tell ReactJS their edentity for lifecycle stuff ~_~
+                   [:a-text {:color "black" :value  msg  :position (str "-1 " (/ idx 3) " -2")}])
+                 @msgs)
     [:a-plane
      {:color "#BBC0C6",
       :height "9000",
@@ -25,7 +26,6 @@
       :rotation "-90 0 0",
       :position "0 0 -4"}]
     [:a-sky {:color "white"}]]])
-            
 
 
 (reagent/render-component [hello-world]
